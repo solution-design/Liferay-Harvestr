35d4e8d40fd0c47943826dd2ea45ac1a3cc7850e
smittex <smittex@gmail.com>
2014-04-14 12:24:26 -0500

    more cleanup

--- docroot/WEB-INF/sql/indexes.sql
+++
@@ -1,2 +0,0 @@
-create index IX_8E63EE33 on RSS_Feed (scope);
-create index IX_4B3FBFEB on RSS_Feed (userId);
--- docroot/WEB-INF/sql/tables.sql
+++
@@ -1,9 +0,0 @@
-create table RSS_Feed (
-	feedId LONG not null primary key,
-	companyId LONG,
-	userId LONG,
-	createDate DATE null,
-	modifiedDate DATE null,
-	url VARCHAR(75) null,
-	scope VARCHAR(75) null
-);
